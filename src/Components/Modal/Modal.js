26439bf812905bf9f321f3c07028cf4ca87d3855
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-12-24 13:55:47 +0200

    add modal

---
+++ src/Components/Modal/Modal.js
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { createPortal } from "react-dom";
+import PropTypes from "prop-types";
+import s from "./Modal.module.css";
+
+const modalRoot = document.querySelector("#modal-root");
+
+export default function Modal({ onCloseModal, children }) {
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeydown);
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  });
+
+  handleKeydown = (e) => {
+    if (e.code === "Escape") {
+      onCloseModal();
+    }
+  };
+
+  handleBackdropClick = (e) => {
+    if (e.currentTarget === e.target) {
+      onCloseModal();
+    }
+  };
+
+  return createPortal(
+    <div className={s.backdrop} onClick={handleBackdropClick}>
+      <div className={s.modal}>{children}</div>
+    </div>,
+    modalRoot
+  );
+}
+
+Modal.propTypes = {
+  onCloseModal: PropTypes.func,
+};
